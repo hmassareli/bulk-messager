172d45c8b73d4777e275c66e1957b89dc1678b6a
Glitch (glitch-hello-node) <none>
2024-01-29 22:21:17 +0000

    🏰🙅 Checkpoint ./src/sendMessages.js:22638318/2426

--- src/sendMessages.js
+++ src/sendMessages.js
@@ -1,49 +1,19 @@
-const sendMessages = (userMessage, client) => {
-  let failedToAnswer = 0;
-  let step = 0;
-  let destinations;
-  client.sendMessage(
-    userMessage.from,
-    "Por favor, insira o(s) número(s) de telefone que deseja enviar mensagens (somente números, e deve incluir 55 e código de área"
-  );
-
+const sendMessages = async (destinations, message, client) => {
   client.on("message", (message) => {
-    let text = message.body.toLowerCase();
-    if (text.includes("55") && message.from === userMessage.from) {
-      step++;
-
-      text = text.replace(/ /g, "");
-      destinations = text.split("\n");
-      client.sendMessage(
-        userMessage.from,
-        "Por favor, insira a mensagem que deseja enviar a esses números"
-      );
-    } else {
-      if (step === 1 && message.from === userMessage.from) {
-        step++;
-        let textToSend = message.body;
-        client.sendMessage(
-          userMessage.from,
-          "Entendido, enviando:\n\n" +
-            text +
-            "\n\n para\n" +
-            destinations.toString()
-        );
-        const getRandomNumber = () => Math.floor(Math.random() * 15000);
-        let randomNumber = 0;
-        // let secondRandomNumber = Math.floor(Math.random() * 2000 + 1000);
-        function sendMessages(line, randomInt) {
-          setTimeout(() => {
-            client.sendMessage(line + "@c.us", textToSend);
-          }, randomInt);
-        }
-        destinations.map((line) => {
-          sendMessages(line, randomNumber);
-          randomNumber += getRandomNumber();
-        });
-      }
-      failedToAnswer++;
+    const getRandomNumber = () => Math.floor(Math.random() * 15000);
+    let randomNumber = 0;
+    // let secondRandomNumber = Math.floor(Math.random() * 2000 + 1000);
+    async function sendMessages(line, randomInt) {
+      setTimeout(() => {
+        client.sendMessage(line + "@c.us", message);
+      }, randomInt);
     }
+    destinations.map((line) => {
+      await sendMessages(line, randomNumber);
+      randomNumber += getRandomNumber();
+    });
+    
+    return 
   });
 };
 module.exports = sendMessages;
